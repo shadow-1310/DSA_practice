066fd660b74be00c6394a9af32da54f7ba951d35
Shekhar <shekharnath9@gmail.com>
2023-08-29 20:35:39 +0530

    added some more revisionsof heap

--- LeetCode/top_interview/Heap/1405.longest_happy_string.py
+++ LeetCode/top_interview/Heap/1405.longest_happy_string.py
@@ -31,3 +31,35 @@ class Solution:
                 heapq.heappush(max_heap, (count, char))
 
         return res
+
+
+class Solution:
+    def longestDiverseString(self, a, b, c):
+        res = ""
+        max_heap = []
+        input_char = [(-a, "a"), (-b, "b"), (-c, "c")]
+
+        for count, char in input_char:
+            if count:
+                heapq.heappush(max_heap, (count, char))
+
+
+        while max_heap:
+            count, char = heapq.heappop(max_heap)
+            if len(res) > 1 and res[-1] == res[-2] == char:
+                if not max_heap:
+                    break
+                count2, char2 = heapq.heappop(max_heap)
+                res += char2
+                count2 += 1
+
+                if count2:
+                    heapq.heappush(max_heap, (count2, char2))
+
+            else:
+                res += char
+                count += 1
+            if count:
+                heapq.heappush(max_heap, (count, char))
+
+        return res
--- LeetCode/top_interview/Heap/378.kth_smallest_in_matrix.py
+++ LeetCode/top_interview/Heap/378.kth_smallest_in_matrix.py
@@ -16,3 +16,22 @@ class Solution:
                 heapq.heappush(min_heap, (matrix[r][c+1], r, c+1))
 
         return ans
+
+# this is a revision, correct solution done on 29-08-2023 
+class Solution:
+    def kthSmallest(self, matrix, k):
+        m = len(matrix)
+        n = len(matrix[0])
+
+        rows = min(k, m)
+        min_heap = []
+        for i, row in enumerate(matrix[:rows]):
+            heapq.heappush(min_heap, (row[0], i, 0))
+
+        for i in range(k):
+            ans, r, c = heapq.heappop(min_heap)
+            if c+1 < n:
+                heapq.heappush(min_heap, (matrix[r][c+1], r, c+1))
+
+        return ans
+
