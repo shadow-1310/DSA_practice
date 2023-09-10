416163d69c2c0bcf6ff9d20c996f92857912c8ee
Shekhar <shekharnath9@gmail.com>
2023-09-10 21:11:20 +0530

    done more array revisions

--- LeetCode/top_interview/array/easy/169.majority_element.py
+++ LeetCode/top_interview/array/easy/169.majority_element.py
@@ -9,6 +9,33 @@ def majority(nums):
     for key in count_map:
         if count_map[key] > n/2:
             return key
+
+
+#this is a revision done on 10-09-2023
+#it is done using Moore's voting algorithm
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        res = nums[0]
+        for num in nums:
+            if count == 0:
+                res = num
+                count += 1
+
+            elif num == res:
+                count += 1
+
+            elif num != res:
+                count -= 1
+
+        if count != 0:
+            count = 0
+            for num in nums:
+                if num == res:
+                    count += 1
+
+        if count > len(nums) / 2:
+            return res
         
 nums = [2,2,1,1,1,2,2]
 print(majority(nums))
--- LeetCode/top_interview/array/easy/283.move_zeros.py
+++ LeetCode/top_interview/array/easy/283.move_zeros.py
@@ -1,3 +1,4 @@
+#this is a wrong solution as it doesnot preserve orderof non zero elements
 def move_zeros(nums):
     n = len(nums) 
     left = 0
@@ -31,6 +32,36 @@ def move_zeros_new(nums):
     for i in range(count):
         nums.append(0)
 
+
+#this is a revision done on 10-09-2023, but not working for all testcases in LC, one failed case is [0,1]
+class Solution:
+    def moveZeroes(self, nums):
+        left = 0
+        right = left
+
+        while left <= right and right < len(nums):
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            if nums[right] == 0:
+                right += 1
+
+
+#this is a revision done on 10-09-2023, working on all testcases of LC
+class Solution:
+    def moveZeroes(self, nums):
+        count = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] == 0:
+                nums.pop(i)
+                count += 1
+
+            else:
+                i += 1
+
+        for i in range(count):
+            nums.append(0)
 
 nums = [0,1,0,3,12]
 nums1 = [0, 0, 1]
