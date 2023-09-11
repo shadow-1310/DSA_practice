01dc882e6a7e445e4595368f2f62c29eabcab5e4
Shekhar <shekharnath9@gmail.com>
2023-09-11 15:00:35 +0530

    added gfg problem

---
+++ company_specific/fractal/arrange_array.py
@@ -0,0 +1,39 @@
+#this is a two pointer approach done on 11-09-2023 but not working for GFG testcases
+class Solution:
+    def Rearrange(self, n, arr):
+        left = 0
+        curr = left
+
+        while curr <= n -1 and left <= n -1:
+            if arr[curr] < 0:
+                arr[left], arr[curr] = arr[curr], arr[left]
+                left += 1
+
+            curr += 1
+
+
+#this is a brute force approach done on 11-09-2023 which is woring for all testcases on GFG
+class Solution:
+    def Rearrange(self, n, arr):
+        pos = []
+        neg = []
+
+        for i in range(n):
+            if arr[i] < 0:
+                neg.append(arr[i])
+            else:
+                pos.append(arr[i])
+
+        idx = 0
+        i = 0
+        while idx < len(neg):
+            arr[idx] = neg[i]
+            idx += 1
+            i += 1
+
+        i = 0
+        while idx < n and i < len(pos):
+            arr[idx] = pos[i]
+            idx += 1
+            i += 1
+
