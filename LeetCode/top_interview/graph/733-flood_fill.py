71ae72c91d8465e66e16546b29849ffbdca05318
Shekhar <shekharnath9@gmail.com>
2023-10-01 16:21:47 +0530

    added graph problem flood fill

---
+++ LeetCode/top_interview/graph/733-flood_fill.py
@@ -0,0 +1,32 @@
+class Solution:
+    def floodFill(self, image, sr, sc, color):
+        rows = len(image)
+        cols = len(image[0])
+        org = image[sr][sc]
+        visited = set()
+        def dfs(r,c):
+            if (
+                    (r,c) in visited or 
+                    (r < 0 or c < 0) or
+                    (r == rows or c == cols) or
+                    image[r][c] != org
+                ):
+                return
+            image[r][c] = color
+            visited.add((r,c))
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+
+        dfs(sr,sc)
+        return image
+
+image = [[1,1,1],
+         [1,1,0],
+         [1,0,1]]
+sr = 1
+sc = 1
+color = 2
+s = Solution()
+print(s.floodFill(image, sr,sc,color))
