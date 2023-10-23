1e8351039bb1de74b572e13c67bc652524f404ea
Shekhar <shekharnath9@gmail.com>
2023-10-23 16:37:02 +0530

    adding some revisions and also new problems

--- LeetCode/top_interview/binary_search/1011-ship_packages.py
+++ LeetCode/top_interview/binary_search/1011-ship_packages.py
@@ -29,9 +29,37 @@ class Solution:
 
         return res
 
+
+#revised on 20-10-2023
+class Solution:
+    def shipWithinDays(self, weight, days):
+        l = 0
+        r = 0
+        for w in weight:
+            r += w
+            l = max(l,w)
+        res = r
+        while l <= r:
+            mid = (l+r) >> 1
+            i = d = 0
+            while i < len(weight):
+                curr = 0
+                while i < len(weight) and (curr + weight[i]) <= mid:
+                    curr += weight[i]
+                    i += 1
+                d += 1
+            if d > days:
+                l = mid+1
+            else:
+                res = min(res, mid)
+                r = mid-1
+        
+        return res
+
+
 # weights = [1,2,3,4,5,6,7,8,9,10]
 # days = 5
-# weights = [3,2,2,4,1,4]; days = 3
 weights = [1,2,3,1,1]; days = 4
+weights = [3,2,2,4,1,4]; days = 3
 s = Solution()
 print(s.shipWithinDays(weights, days))
--- LeetCode/top_interview/binary_search/74.search_2D_matrix.py
+++ LeetCode/top_interview/binary_search/74.search_2D_matrix.py
@@ -67,8 +67,40 @@ class Solution:
         return False
 
 
+class Solution:
+    def searchMatrix(self, matrix, target):
+        rows = len(matrix)
+        cols = len(matrix[0])
+        l = 0
+        r = rows-1
+
+        while l <= r:
+            mid = (l+r) >> 1
+            if matrix[mid][0] == target:
+                return True
+            elif matrix[mid][0] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        row = r
+        l = 0
+        r = cols-1
+
+        while l <= r:
+            mid = (l+r) >> 1
+            if matrix[row][mid] == target:
+                return True
+            elif matrix[row][mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return False
+
 matrix = [[1,2,5,7],[10,11,16,20],[23,30,34,60]] 
-target = 3
+target = 60
 
 print(serach(matrix, target))
-# print(len(matrix))
+s = Solution()
+print(s.searchMatrix(matrix, target))
